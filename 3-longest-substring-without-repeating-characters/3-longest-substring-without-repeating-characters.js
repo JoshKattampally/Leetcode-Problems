aca828a71784da0460e0b44c16f05907458a4cbd
Josh Kattampally <77760014+JoshKattampally@users.noreply.github.com>
2022-05-17 09:26:11 -0700

    Time: 116 ms (66.92%), Space: 44.6 MB (97.15%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,16 @@
+var lengthOfLongestSubstring = function(s) {
+  var start = 0, maxLen = 0;
+  var map = new Map();
+
+  for(var i = 0; i < s.length; i++) {
+      var ch = s[i];
+    
+      if(map.get(ch) >= start) start = map.get(ch) + 1;
+      map.set(ch, i);
+    
+      maxLen = Math.max(maxLen, i - start + 1);
+
+  }
+
+  return maxLen;
+};
